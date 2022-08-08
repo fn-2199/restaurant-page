8cd779c40a4c523197f0d2983f228e648cf978dd
Pat <alfonsopatrisha@gmail.com>
2022-08-08 14:43:00 -0700

    Implement html webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './style.css';
 
 console.log("Hello world");
+
+function component() {
+    const element = document.createElement('div');
+  
+    element.innerHTML = 'Hello webpack';
+    
+    return element;
+}
+  
+document.body.appendChild(component());
