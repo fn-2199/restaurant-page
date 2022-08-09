cbb2c50e28b1b896892a99b4c0e4592ded471dcc
Pat <alfonsopatrisha@gmail.com>
2022-08-08 17:41:06 -0700

    Create a template for our main HTML file

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist'
+  entry: './src/index.js',  //input
+  devtool: 'inline-source-map', //to track down errors
+  devServer: {  //for webpack-dev-server
+    static: './dist' //indicates which directory will be hosted
   },
-  plugins: [new HtmlWebpackPlugin({title: 'Barcada'})],
-  output: {
+  plugins: [new HtmlWebpackPlugin({ //generates new index.html and injects all generated bundles
+    hash: true,
+    title: 'Barcada',
+    template: './src/index.html'
+})],
+  output: { //outputs bundled file
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true //cleans up dist folder
   },
-  module: {rules: [{test: /\.css$/i, use: ['style-loader', 'css-loader']},
+  module: {rules: [{test: /\.css$/i, use: ['style-loader', 'css-loader']},  //loaders
     {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource'},
     {test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource'}
   ]}
