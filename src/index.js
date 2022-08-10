d062996e59117500403be3c6cd19ac7673aa99a4
Pat <alfonsopatrisha@gmail.com>
2022-08-09 17:53:35 -0700

    Add background image

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import './style.css';
 
 console.log("Hello world");
-
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = 'Hello webpack';
-    
-    return element;
-}
-  
-document.body.appendChild(component());
