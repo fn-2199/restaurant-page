5f9057e9c54d557ee32123a66ebe64ced76c50f0
Pat <alfonsopatrisha@gmail.com>
2022-08-10 18:06:36 -0700

    Add onclick function

--- src/index.js
+++ src/index.js
@@ -4,23 +4,32 @@ import MENU from './menu';
 import RESERVE from "./reserve";
 import {contactObj, CONTACT} from "./contact";
 
-const content = document.querySelector('.content');
+//Make imported functions global
+window.HOME = HOME;
+window.MENU = MENU;
+window.RESERVE = RESERVE;
+window.CONTACT = CONTACT;
 
 const generateFrontPage = function() {
-    const navArray = ['HOME', 'MENU', 'RESERVE', 'CONTACT'];
+    const content = document.querySelector('.content');
+
+    //Create navigation
     const nav = document.createElement('nav');
     const ul = document.createElement('ul');
 
+    const navArray = ['HOME', 'MENU', 'RESERVE', 'CONTACT'];
     for (let page of navArray) {
         const li = document.createElement('li');
         li.textContent = page;
+        li.onclick = activatePage;
         ul.appendChild(li);
     }
 
     nav.appendChild(ul);
 
+    //Create footer
     const footer = document.createElement('footer');
-
+    
     for (let info of Object.values(contactObj)) {
         const p = document.createElement('p');
         p.textContent = info;
@@ -28,28 +37,11 @@ const generateFrontPage = function() {
     }
 
     content.append(nav, HOME(), footer)
-
 }();
 
-const navLinks = document.querySelectorAll('nav > ul > li')
-navLinks.forEach((nav) => {nav.addEventListener("click", function() {
-    //Removes active page element
+function activatePage() {
+    //Remove current active page
     (document.querySelector('.content').firstChild.nextSibling).remove();
-
-    //Generates selected page
-    switch (nav.textContent) {
-        case 'HOME':
-            content.insertBefore(HOME(), document.querySelector('footer'));
-            break;
-        case 'MENU':
-            content.insertBefore(MENU(), document.querySelector('footer'));
-            break;
-        case 'RESERVE':
-            content.insertBefore(RESERVE(), document.querySelector('footer'));
-            break;
-        case 'CONTACT':
-            content.insertBefore(CONTACT(), document.querySelector('footer'));
-            break;
-    }
-
-})})
+    //Display new selected page
+    document.querySelector('.content').insertBefore(window[`${this.textContent}`](), document.querySelector('footer'));
+}
