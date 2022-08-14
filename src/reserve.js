02c2d2b5f16ad370173c75405a30f96c9bcd23d2
Pat <alfonsopatrisha@gmail.com>
2022-08-14 15:40:57 -0700

    Add all fields for the form

--- src/reserve.js
+++ src/reserve.js
@@ -4,7 +4,15 @@ const formElements = (camelCase, stringLabel, inputType) => {
     return {camelCase, stringLabel, inputType}
 }
 
-let formArray = [formElements('firstName', "First Name", 'text'), formElements('lastName', "Last Name", 'text'), formElements('phoneNumber', "Phone Number", 'tel'), formElements('email', "Email", 'email'), formElements('numofPpl', "Number of Party", 'number'), formElements('dateTime', "Date & Time", 'datetime-local')];
+const categoryElements = (title, array, camelCase) => {
+    return {title, array, camelCase}
+}
+
+let basicInfoArray = [formElements('firstName', "First Name", 'text'), formElements('lastName', "Last Name", 'text'), formElements('phoneNumber', "Phone Number", 'tel'), formElements('email', "Email", 'email'), formElements('numofPpl', "Number of Party", 'number'), formElements('dateTime', "Date & Time", 'datetime-local')];
+
+let paymentInfoArray = [formElements('cardNumber', "Card Number", 'tel'), formElements('expirationDate', "Expiration Date", 'month'), formElements('cvv', "CVV", 'tel'), formElements('zipCode', "Card Zip Code", 'text')];
+
+let formCategoryArray = [categoryElements("Basic Information", basicInfoArray, 'basic-info'), categoryElements("Entrees & Comments", entreesItemsArray, 'entrees-comments'), categoryElements("Payment Information", paymentInfoArray, 'payment-info')];
 
 function generateReserve() {
     const form = document.createElement('form');
@@ -17,49 +25,65 @@ function generateReserve() {
     h3.textContent = 'Reservation Form';
 
     form.append(h3);
+
+    formCategoryArray.forEach((category) => {
+        const fieldset = document.createElement('fieldset');
+        fieldset.id = category.camelCase;
+        const legend = document.createElement('legend');
+        legend.textContent = category.title;
+        fieldset.append(legend);
+        form.append(fieldset);
+
+        if (category.title == 'Basic Information' || category.title == 'Payment Information') {
+            for (let element of category.array) {
+                const div = document.createElement('div');
     
-    for (let element of formArray) {
-        const div = document.createElement('div');
+                const label = document.createElement('label');
+                label.textContent = element.stringLabel;
+    
+                const strong = document.createElement('strong');
+                const abbr = document.createElement('abbr');
+                abbr.title = 'required';
+                abbr.textContent = '*';
+    
+                const input = document.createElement('input');
+                input.type = element.inputType;
+                input.name = element.camelCase;
+                input.setAttribute('required', '');
+    
+                strong.appendChild(abbr);
+                label.append(strong, input);
+                div.append(label);
+                fieldset.appendChild(div);
+            }
+        } else {
+            for (let entree of category.array) {
+                const label = document.createElement('label');
+                const span = document.createElement('span');
+                span.textContent = entree.foodName;
+    
+                const input = document.createElement('input');
+                input.type = 'checkbox';
+                input.name = 'entreeItem';
+                input.value = entree.camelCase;
+    
+                label.append(input, span);
+                fieldset.appendChild(label);
+            }
 
-        const label = document.createElement('label');
-        label.setAttribute('for', `${element.camelCase}`);
+            const div = document.createElement('div');
+            const label = document.createElement('label');
+            label.textContent = "Add-Ons/Comments/Requests/Allergies/Celebration";
 
-        const span = document.createElement('span');
-        span.textContent = element.stringLabel;
+            const textarea = document.createElement('textarea');
+            textarea.name = 'comments';
 
-        const strong = document.createElement('strong');
-        const abbr = document.createElement('abbr');
-        abbr.title = 'required';
-        abbr.textContent = '*';
+            label.append(textarea);
+            div.appendChild(label);
+            fieldset.appendChild(div);
 
-        const input = document.createElement('input');
-        input.type = element.inputType;
-        input.id = input.name = element.camelCase;
-        input.setAttribute('required', '');
-
-        strong.appendChild(abbr);
-        label.append(span, strong);
-        div.append(label, input);
-        form.appendChild(div);
-    }
-
-    for (let entree of entreesItemsArray) {
-        const label = document.createElement('label');
-        label.setAttribute('for', `${entree.camelCase}`);
-        label.textContent = entree.foodName;
-
-        const input = document.createElement('input');
-        input.type = 'checkbox';
-        input.name = 'entreeItem';
-        input.id = input.value = entree.camelCase;
-
-        form.append(input, label)
-    }
-
-    // Create checkbox fieldset
-
-    const fieldset = document.createElement('fieldset');
-    const legend = document.createElement('legend');
+        } 
+    })
 
     return form;
 }
