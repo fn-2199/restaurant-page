56683497059cdece5c4d98c3866057f242e286ed
Pat <alfonsopatrisha@gmail.com>
2022-08-14 17:15:11 -0700

    Change array declaration type to const

--- src/reserve.js
+++ src/reserve.js
@@ -8,11 +8,11 @@ const categoryElements = (title, array, camelCase) => {
     return {title, array, camelCase}
 }
 
-let basicInfoArray = [formElements('firstName', "First Name", 'text'), formElements('lastName', "Last Name", 'text'), formElements('phoneNumber', "Phone Number", 'tel'), formElements('email', "Email", 'email'), formElements('numOfPpl', "Number of Party", 'number'), formElements('dateTime', "Date & Time", 'datetime-local')];
+const basicInfoArray = [formElements('firstName', "First Name", 'text'), formElements('lastName', "Last Name", 'text'), formElements('phoneNumber', "Phone Number", 'tel'), formElements('email', "Email", 'email'), formElements('numOfPpl', "Number of Party", 'number'), formElements('dateTime', "Date & Time", 'datetime-local')];
 
-let paymentInfoArray = [formElements('cardNumber', "Card Number", 'tel'), formElements('expirationDate', "Expiration Date", 'month'), formElements('cvv', "CVV", 'tel'), formElements('zipCode', "Card Zip Code", 'text')];
+const paymentInfoArray = [formElements('cardNumber', "Card Number", 'tel'), formElements('expirationDate', "Expiration Date", 'month'), formElements('cvv', "CVV", 'tel'), formElements('zipCode', "Card Zip Code", 'text')];
 
-let formCategoryArray = [categoryElements("Basic Information", basicInfoArray, 'basic-info'), categoryElements("Entrees & Comments", entreesItemsArray, 'entrees-comments'), categoryElements("Payment Information", paymentInfoArray, 'payment-info')];
+const formCategoryArray = [categoryElements("Basic Information", basicInfoArray, 'basic-info'), categoryElements("Entrees & Comments", entreesItemsArray, 'entrees-comments'), categoryElements("Payment Information", paymentInfoArray, 'payment-info')];
 
 function generateReserve() {
     const form = document.createElement('form');
@@ -23,8 +23,7 @@ function generateReserve() {
 
     const h3 = document.createElement('h3');
     h3.textContent = 'Reservation Form';
-
-    form.append(h3);
+    form.appendChild(h3);
 
     formCategoryArray.forEach((category) => {
         const fieldset = document.createElement('fieldset');
@@ -95,8 +94,8 @@ function generateReserve() {
     })
 
     const button = document.createElement('button');
-    button.textContent = "Reserve Table";
-    form.append(button);
+    button.textContent = "Submit";
+    form.appendChild(button);
 
     return form;
 }
